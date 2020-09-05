6efeef517bd55a799b546e912c10f8b92dbc5abc
BIKOU <bikouyehkachristaloys@gmail.com>
2020-09-05 14:29:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myfragmentactivitykotlin/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.myfragmentactivitykotlin
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    var tabLayout: TabLayout? = null
+    var frameLayout: FrameLayout? = null
+    var fragment: Fragment? = null
+    var fragmentManager: FragmentManager? = null
+    var fragmentTransaction: FragmentTransaction? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        frameLayout = findViewById<FrameLayout>(R.id.frameLayout)
+
+        fragment = JavaFragment()
+        fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager!!.beginTransaction()
+        fragmentTransaction!!.replace(R.id.frameLayout, fragment)
+        fragmentTransaction!!.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        fragmentTransaction!!.commit()
+        //adding listener for tab select
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                // creating cases for fragment
+                when (tab.position) {
+                    0 -> fragment = JavaFragment()
+                    1 -> fragment = AndroidFragment()
+                    2 -> fragment = KotlinFragment()
+                    3 -> fragment = PhpFragment()
+                    4 -> fragment = PythonFragment()
+                }
+                val fm = supportFragmentManager
+                val ft = fm.beginTransaction()
+                ft.replace(R.id.frameLayout, fragment)
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                ft.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
+    }
+}
