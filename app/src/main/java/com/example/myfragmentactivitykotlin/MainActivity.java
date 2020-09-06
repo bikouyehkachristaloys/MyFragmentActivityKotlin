cd9e633b08a5afe457b07715c37ea5c9a8ea77b7
BIKOU <bikouyehkachristaloys@gmail.com>
2020-09-05 17:11:01 -0700

    Phoenix 3

--- app/src/main/java/com/example/myfragmentactivitykotlin/MainActivity.java
+++ app/src/main/java/com/example/myfragmentactivitykotlin/MainActivity.java
@@ -1,6 +1,32 @@
 package com.example.myfragmentactivitykotlin;
 
 import android.app.Activity;
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_android.view.*
 
 public class MainActivity extends Activity {
+
+    class fragment1 : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                savedInstanceState: Bundle?): View? {
+                // Inflate the layout for this fragment
+                val view: View = inflater!!.inflate(R.layout.fragment_java, container, false)
+
+        view.androidButton!!.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context,"java fragment", Toast.LENGTH_SHORT).show()
+        })
+        return view
+        }
+    }// Required empty public constructor
+
 }
+
+
+
+
